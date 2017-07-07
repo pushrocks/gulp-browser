d3069ebf5fe5ecef38e7b9293a57c9c9d1dae027
Phil Kunz <gitlab@philkunz.com>
2017-07-07 18:32:37 +0200

    update to latest standards

--- dist/gulpbrowser.plugins.js
+++ dist/gulpbrowser.plugins.js
@@ -1,6 +1,7 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 require("typings-global");
 exports.through2 = require("through2");
 exports.path = require("path");
 exports.browserify = require("browserify");
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZ3VscGJyb3dzZXIucGx1Z2lucy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uL3RzL2d1bHBicm93c2VyLnBsdWdpbnMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLDBCQUF1QjtBQUN2Qix1Q0FBNEM7QUFDNUMsK0JBQW9DO0FBQ3BDLDJDQUFnRCJ9
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZ3VscGJyb3dzZXIucGx1Z2lucy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uL3RzL2d1bHBicm93c2VyLnBsdWdpbnMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSwwQkFBdUI7QUFDdkIsdUNBQTRDO0FBQzVDLCtCQUFvQztBQUNwQywyQ0FBZ0QifQ==
--- test/test.ts
+++ test/test.ts
@@ -1,62 +1,66 @@
-import 'typings-test'
-
 import * as beautylog from 'beautylog'
 import * as gulp from 'gulp'
-import * as gulpBrowser from '../dist/index.js'
 import * as gulpFunction from 'gulp-function'
+import * as smartq from 'smartq'
 
-import * as should from 'should'
-import * as q from 'q'
+import { expect, tap } from 'tapbundle'
 
+import * as gulpBrowser from '../dist/index.js'
 
-describe('gulpBrowser', function () {
-    describe('.browserify', function () {
-        it('should run through smoothly', function (done) {
-            this.timeout(30000)
-            let stream = gulp.src('./test/testBrowserifyNormal.js')
-                .pipe(gulpBrowser.browserify())
-                .pipe(gulp.dest('./test/result/'))
-                .pipe(gulpFunction.atEnd(() => {
-                    let done2 = q.defer()
-                    done()
-                    done2.resolve()
-                    return done2.promise
-                }))
-        })
-
-        it('should throw an error, when a module is not found', function (done) {
-            let d = require('domain').create()
-            let doneCalled = false
-            d.on('error',function(error){
-                if (!doneCalled) {
-                    done()
-                    doneCalled = true
-                }
-            })
-            d.run(function() {
-                let stream = gulp.src('./test/testBrowserifyError.js')
-                    .pipe(gulpBrowser.browserify())
-            })
-        })
-
-        it('should run through with an empty file', function (done) {
-            let stream = gulp.src('./test/testBrowserifyEmpty.js')
-                .pipe(gulpBrowser.browserify())
-                .pipe(gulp.dest('./test/result/'))
-                .pipe(gulpFunction.atEnd(done))
-        })
-        it('should run through work with transforms', function (done) {
-            this.timeout(30000)
-            let transforms = [
-                {
-                    transform: 'babelify',
-                    options: {presets: ['es2015']}
-                }
-            ]
-            let stream = gulp.src('./test/testBrowserifyNormal.js')
-                .pipe(gulpBrowser.browserify(transforms))
-                .pipe(gulp.dest('./test/result/'))
-                .pipe(gulpFunction.atEnd(done))
-        })
-    })
+tap.test('should run through smoothly', async () => {
+  let done = smartq.defer()
+  let stream = gulp.src('./test/testBrowserifyNormal.js')
+    .pipe(gulpBrowser.browserify())
+    .pipe(gulp.dest('./test/result/'))
+    .pipe(gulpFunction.atEnd(async () => {
+      done.resolve()
+    }))
+  await done.promise
 })
+
+tap.test('should throw an error, when a module is not found', async () => {
+  let done = smartq.defer()
+  let d = require('domain').create()
+  let doneCalled = false
+  d.on('error', function (error) {
+    if (!doneCalled) {
+      done.resolve()
+      doneCalled = true
+    }
+  })
+  d.run(function () {
+    let stream = gulp.src('./test/testBrowserifyError.js')
+      .pipe(gulpBrowser.browserify())
+  })
+  await done.promise
+})
+
+tap.test('should run through with an empty file', async () => {
+  let done = smartq.defer()
+  let stream = gulp.src('./test/testBrowserifyEmpty.js')
+    .pipe(gulpBrowser.browserify())
+    .pipe(gulp.dest('./test/result/'))
+    .pipe(gulpFunction.atEnd(async () => {
+      done.resolve()
+    }))
+  await done.promise
+})
+
+tap.test('should run through work with transforms', async () => {
+  let done = smartq.defer()
+  let transforms = [
+    {
+      transform: 'babelify',
+      options: { presets: [ 'es2015' ] }
+    }
+  ]
+  let stream = gulp.src('./test/testBrowserifyNormal.js')
+    .pipe(gulpBrowser.browserify(transforms))
+    .pipe(gulp.dest('./test/result/'))
+    .pipe(gulpFunction.atEnd(async () => {
+      done.resolve()
+    }))
+  await done.promise
+})
+
+tap.start()
